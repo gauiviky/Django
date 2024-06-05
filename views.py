3b2fcdfe077b3e060dd0d892af73ccddff414cdf
gauiviky <159021289+gauiviky@users.noreply.github.com>
2024-06-05 10:01:28 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def wish(request):
+    #messege="<h1>THE HEASD</h1>"
+    return HttpResponse('messege')
+
+# Create your views here.
+
+#http://127.0.0.1:8000/wish-response
+
